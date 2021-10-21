06093a0e1b8c0309f155545c335e633173cecf1c
RogerioGNunes <rg.nunes2013@gmail.com>
2021-10-21 19:57:27 +0000

    Herança 2a aula

---
+++ Pessoa.java
@@ -0,0 +1,18 @@
+public class Pessoa{ 
+    private String nome;
+    private String telefone;
+    private String endereco;
+// fazer os métodos de acesso público
+// set
+public void setNome(String nome){
+    this.nome=nome;
+}
+// get
+public String getNome(){
+    return this.nome;
+}
+// set
+public void setTelefone(String telefone){
+    this.telefone=telefone;
+}
+}
